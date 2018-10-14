122a682ffa7269f64f6f982b8bd9eb382afcde8f
kehaomin <1808911343@qq.com>
2018-10-14 18:48:18 +0800

    修改了java代码

--- git-test/src/com/itheima/git/gitClass.java
+++ git-test/src/com/itheima/git/gitClass.java
@@ -7,5 +7,10 @@ public class gitClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //test
+        //test
+        //test
+        //test
+
     }
 }
