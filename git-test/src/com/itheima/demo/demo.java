34167ae2cac2374a1d9f2bd29fd1d7d2ad1ccab5
kehaomin <1808911343@qq.com>
2018-10-15 10:26:20 +0800

    编写了demo方法

---
+++ git-test/src/com/itheima/demo/demo.java
@@ -0,0 +1,13 @@
+package com.itheima.demo;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+    }
+}
